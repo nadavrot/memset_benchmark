a658eb6ed1e4dcf2f111f66516f8bc426e2fd1f0
Nadav Rotem <nadav256@gmail.com>
2021-11-08 15:54:18 -0800

    Benchmark libc memset

---
+++ src/calls.s
@@ -0,0 +1,6 @@
+.text
+.global libc_memset
+.p2align  4, 0x90
+libc_memset:
+        call memset
+
--- src/main.cc
+++ src/main.cc
@@ -6,6 +6,11 @@
 
 #define ITER (1000 * 1000 * 100)
 
+extern "C" {
+void *memset(void *s, int c, size_t n);
+void *libc_memset(void *s, int c, size_t n);
+}
+
 /// Alligns the pointer \p ptr, to alignment \p alignment and offset \p offset
 /// within the word.
 void *align_pointer(void *ptr,unsigned alignment, unsigned offset) {
@@ -26,11 +31,22 @@ std::string params_to_entry(const std::string &name, unsigned size,
 void test(unsigned SIZE, unsigned ALIGN, unsigned OFFSET) {
   TimerGuard T(params_to_entry("simple", SIZE, ALIGN, OFFSET), SIZE);
   std::vector<char> memory(SIZE + 256, 0);
-
   void *ptr = align_pointer(&memory[0], ALIGN, OFFSET);
 
   for (int i = 0; i < ITER; i++) {
     memset(ptr, 0, SIZE);
+  }
+}
+
+
+// Allocate memory and benchmark a single implementation.
+void posix(unsigned SIZE, unsigned ALIGN, unsigned OFFSET) {
+  TimerGuard T(params_to_entry("posix", SIZE, ALIGN, OFFSET), SIZE);
+  std::vector<char> memory(SIZE + 256, 0);
+  void *ptr = align_pointer(&memory[0], ALIGN, OFFSET);
+
+  for (int i = 0; i < ITER; i++) {
+    libc_memset(ptr, 0, SIZE);
   }
 }
 
@@ -40,11 +56,10 @@ int main(int argc, char **argv) {
   test(1000, 16, 0);
   test(2000, 16, 0);
   test(3000, 16, 0);
-  test(1000, 16, 0);
-  test(2000, 16, 0);
-  test(3000, 16, 0);
-  test(1000, 16, 1);
-  test(2000, 16, 1);
-  test(3000, 16, 1);
+
+  posix(1000, 16, 0);
+  posix(2000, 16, 0);
+  posix(3000, 16, 0);
+
   return 0;
 }
