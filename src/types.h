f8387d921e35b00f0bd4bbdc324e6ac5ac0ba57d
Nadav Rotem <nrotem@fb.com>
2021-11-10 14:58:56 -0800

    Align the clang vectors.

--- src/types.h
+++ src/types.h
@@ -4,12 +4,11 @@
 #define NO_INLINE __attribute__((noinline))
 
 #ifdef __clang__
-typedef char char8 __attribute__((ext_vector_type(8)));
-typedef char char16 __attribute__((ext_vector_type(16)));
-typedef char char32 __attribute__((ext_vector_type(32)));
-#endif
-
-#ifdef __GNUC__
+typedef char char8 __attribute__((ext_vector_type(8), aligned(1)));
+typedef char char16 __attribute__((ext_vector_type(16), aligned(1)));
+typedef char char32 __attribute__((ext_vector_type(32), aligned(1)));
+#else
+// __GNUC__
 typedef char char8 __attribute__((vector_size(8), aligned(1)));
 typedef char char16 __attribute__((vector_size(16), aligned(1)));
 typedef char char32 __attribute__((vector_size(32), aligned(1)));
