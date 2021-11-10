f6ad8901ac298f62e58cd47d17d7b6b971ee05d9
Nadav Rotem <nadav256@gmail.com>
2021-11-10 09:16:39 -0800

    Add the char8 type

--- src/types.h
+++ src/types.h
@@ -4,11 +4,13 @@
 #define NO_INLINE __attribute__((noinline))
 
 #ifdef __clang__
+typedef char char8 __attribute__((ext_vector_type(8)));
 typedef char char16 __attribute__((ext_vector_type(16)));
 typedef char char32 __attribute__((ext_vector_type(32)));
 #endif
 
 #ifdef __GNUC__
+typedef char char8 __attribute__((vector_size(8), aligned(1)));
 typedef char char16 __attribute__((vector_size(16), aligned(1)));
 typedef char char32 __attribute__((vector_size(32), aligned(1)));
 #endif
