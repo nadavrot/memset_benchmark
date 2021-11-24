acfb3b1f2eb2409e2d42869e6ec9e7f5b64d4655
Nadav Rotem <nadav256@gmail.com>
2021-11-23 17:10:25 -0800

    [util] Record both memset and memcpy len parameters..

--- src/utils/hist_tool.c
+++ src/utils/hist_tool.c
@@ -1,15 +1,21 @@
 #include <stddef.h>
 #include <stdint.h>
 #include <stdio.h>
+#include <unistd.h>
 
 ////////////////////////////////////////////////////////////////////////////////
-/// This is a small utility that records calls to memset and creates a histogram
-/// of the lengths of calls to memset. It prints the histogram when the program
-/// is terminated. The shared object can be loaded using LD_PRELOAD (on Linux)
-/// or DYLD_INSERT_LIBRARIES (on Mac).
+/// This is a small utility that records calls to some methods and creates a
+/// histogram of the lengths of calls to memset. It prints the histogram when
+/// the program is terminated. The shared object can be loaded using LD_PRELOAD
+/// (on Linux) or DYLD_INSERT_LIBRARIES (on Mac).
 ////////////////////////////////////////////////////////////////////////////////
 
-uint32_t counts[32] = { 0, };
+uint32_t memset_len_dist[32] = {
+    0,
+};
+uint32_t memcpy_len_dist[32] = {
+    0,
+};
 
 const int tab32[32] = {0,  9,  1,  10, 13, 21, 2,  29, 11, 14, 16,
                        18, 22, 25, 3,  30, 8,  12, 20, 28, 15, 17,
@@ -24,16 +30,33 @@ int log2_32(uint32_t value) {
   return tab32[(uint32_t)(value * 0x07C4ACDD) >> 27];
 }
 
-void __attribute__((destructor)) print_hitogram() {
-  printf("Memset histogram:\n");
-  int i;
-  for (i = 0; i < 30; i++) {
-    fprintf(stderr, "%d, %d\n", i, counts[i]);
+void __attribute__((destructor)) print_hitograms() {
+  FILE *ff = fopen("/tmp/hist.txt", "a+");
+  if (!ff) {
+    return;
   }
+  pid_t pid = getpid();
+
+  fprintf(ff, "Histogram for (%d):\n", pid);
+  fprintf(ff, "size, memset, memcpy:\n");
+  for (int i = 0; i < 32; i++) {
+    fprintf(ff, "%d, %d, %d,\n", i, memset_len_dist[i], memcpy_len_dist[i]);
+  }
+  fclose(ff);
+}
+
+void *memcpy(void *dest, const void *src, size_t len) {
+  memcpy_len_dist[log2_32(len)]++;
+  char *d = (char *)dest;
+  char *s = (char *)src;
+  for (size_t i = 0; i < len; i++) {
+    d[i] = s[i];
+  }
+  return dest;
 }
 
 void *memset(void *s, int c, size_t len) {
-  counts[log2_32(len)]++;
+  memset_len_dist[log2_32(len)]++;
   char *p = s;
 
   for (int i = 0; i < len; i++) {
