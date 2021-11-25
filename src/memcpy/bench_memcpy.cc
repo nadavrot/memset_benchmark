9fb8a03ac98748e69b125d27c77b41b0433f5b96
Nadav Rotem <nadav256@gmail.com>
2021-11-24 19:33:59 -0800

    [bench] Add a benchmark of memcpy at random offsets.

--- src/memcpy/bench_memcpy.cc
+++ src/memcpy/bench_memcpy.cc
@@ -48,6 +48,33 @@ void bench_impl(const std::vector<memcpy_ty *> &toTest, unsigned size,
   std::cout << std::endl;
 }
 
+/// Allocate and copy buffers at random offsets and in random sizes.
+/// The sizes and the offsets are in the range 0..256.
+void bench_rand_range(const std::vector<memcpy_ty *> &toTest) {
+  std::vector<char> dest(4096, 1);
+  std::vector<char> src(4096, 0);
+  const char *src_p = &src[0];
+  char *dest_p = &dest[0];
+
+  for (auto handle : toTest) {
+    Stopwatch T;
+    sleep(1);
+    for (unsigned i = 0; i < SAMPLES; i++) {
+      RNG.rand_reset();
+      T.start();
+      for (size_t j = 0; j < ITER; j++) {
+        char *to = dest_p + RNG.next_u8_random();
+        const char *from = src_p + RNG.next_u8_random();
+        (handle)(to, from, RNG.next_u8_random());
+      }
+      T.stop();
+    }
+
+    std::cout << T.get_median() << ", ";
+  }
+  std::cout << std::endl;
+}
+
 int main(int argc, char **argv) {
   std::cout << std::setprecision(3);
   std::cout << std::fixed;
@@ -55,8 +82,13 @@ int main(int argc, char **argv) {
   std::vector<memcpy_ty *> toTest = {&memcpy, &__folly_memcpy, &local_memcpy,
                                      &asm_memcpy};
 
+  std::cout << "Batches of random sizes:\n";
+  std::cout << "size, libc-direct, folly, c_memcpy, asm_memcpy\n";
+
+  bench_rand_range(toTest);
+
   std::cout << "\nFixed size:\n";
-  std::cout << "size, libc-direct, folly, naive_memcpy, asm_memcpy\n";
+  std::cout << "size, libc-direct, folly, c_memcpy, asm_memcpy\n";
 
   for (int i = 0; i < 512; i++) {
     bench_impl(toTest, i, 16, 0);
